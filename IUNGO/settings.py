13c70bac3125e7a8d121c543d5fc8c2849415024
janaki-jeevan1 <jeevan@iungoadvantec.com>
2019-12-27 12:56:16 +0530

    static changes

--- IUNGO/settings.py
+++ IUNGO/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '06#ybpbahjky2xm)-=s!4*743_t!62cl#8re*^bfp1l)#d(i61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- IUNGO/urls.py
+++ IUNGO/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^client/', include('ClientDashboard.urls')),
     url(r'^', include('website.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
